ce3ed778f7ec326821417744e6328cb20fcde3c9
Daniel Wang <danielwang0989@gmail.com>
2025-01-23 11:33:11 -0800

    add

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php $title = 'Home'; ?>
 
 <div class="container">
-    <h3>Daniel Wang</h3>
+    <h3 class="text-success">Daniel Wang</h3>
     <div class="mt-4 p-2 bg-secondary text-white rounded" style="width: 60%;">
         <h3>Check your zodiac.</h3>
         <p>
